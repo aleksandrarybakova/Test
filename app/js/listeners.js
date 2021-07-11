5e23265a9a9dc60b99a33c8e4275c9bbbc058a33
aleksandrarybakova <sashik1988@yandex.ru>
2021-07-11 17:29:08 +0300

    Test: fix

--- app/js/listeners.js
+++ app/js/listeners.js
@@ -1,31 +1,4 @@
-caretCity.addEventListener("click", function(){
-	if(cityList.classList.contains('transition-down')){
-		header.close_city_list()
-	}else{
-		
-		header.parse_city_list()
-	}	  
-})
-
-cityForSelect.forEach(function(el){
-	el.addEventListener('click', function () {
-		header.select_city(el)
-	});
-});
-
-vacancyCaret.addEventListener("click", function(){
-	if(vacancyList.classList.contains('transition-down')){
-		vacancy.close_vacancy_list()
-	}else{
-		
-		vacancy.parse_vacancy_list()
-	}	  
-})
-
-vacancyForSelect.forEach(function(el){
-	el.addEventListener('click', function () {
-		vacancy.select_vacancy(el)
-	});
-});
-
+add_more.add_link.click(add_more.create_add_form);
+choose_quant.choose_item_wrap.click(choose_quant.change_colors);
+$('#choise_continue').click(chosen_list.show_chosen_list)
 
